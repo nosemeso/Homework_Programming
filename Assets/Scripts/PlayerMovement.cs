a837a52dc3f0b54ade92b3525dedef7565d004c1
nosemeso <nosemeso8@gmail.com>
2025-10-18 13:19:34 -0400

    10/18/25

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,5 +2,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody2D;
 
+    private float _xspeed;
+    private float _yspeed;
+    public float speed = 3;
+    private string Input = "horizontal";
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>(); 
+    }
+
+    private void Update()
+    {
+        _xspeed = Input.GetAxis("horizontal");
+        _xspeed = Input.GetAxis("Vertical");
+        _rigidbody2D.velocity = Vector2(_xspeed, _yspeed) * speed;
+    }
 }
